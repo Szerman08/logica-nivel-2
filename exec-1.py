b03f258e6d6f1fe9492d76cb51ff3bbe8a996df5
Caio <120686918+Szerman08@users.noreply.github.com>
2024-09-27 19:09:40 -0300

    exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,12 @@
+# Inicializa as variáveis
+A = 10
+B = 20
+
+# Troca os valores usando uma variável temporária
+Temp = A
+A = B
+B = Temp
+
+# Exibe os valores após a troca
+print("A:", A)
+print("B:", B)
