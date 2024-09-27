8a8e302a7ea20c2a00c48ef72e9872d2d5a10eff
Caio <120686918+Szerman08@users.noreply.github.com>
2024-09-27 19:38:38 -0300

    exec-6.py

---
+++ exec-6.py
@@ -0,0 +1,8 @@
+#Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+
+Base = float(input("Qual o valor da base do retangulo ? "))
+Altura = float(input("Qual a altura do retangulo ? "))
+
+area = Base * Altura
+
+print(area)
