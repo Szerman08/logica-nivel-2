324a9c72d08382de3d5639844866cbb1077519cf
Caio <120686918+Szerman08@users.noreply.github.com>
2024-09-27 19:31:40 -0300

    exec-5.py

---
+++ exec-5.py
@@ -0,0 +1,6 @@
+#Escreva um algoritmo para ler um valor (do teclado) e escrever
+# (na tela) o seu antecessor.
+
+#ANTECESSOR DE UM NÚMERO :
+n1 = int(input('Digite um número: '))
+print(f'O antecessor de {n1} é {n1 - 1}')
