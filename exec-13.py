2902dc45fb2dc817e04c60d1e97d725fbf197b1d
Caio <120686918+Szerman08@users.noreply.github.com>
2024-10-29 18:52:40 -0300

    exec-13.py

---
+++ exec-13.py
@@ -0,0 +1,9 @@
+#13) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+valor = float(input("Digite um valor :"))
+if valor < 10:
+        print("O numero informado é menor que 10")
+else:
+        print('O numero informado é maior que 10')
+
+
