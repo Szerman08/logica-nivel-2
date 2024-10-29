c0a5cb0d4aeb99c90cb42e9737b87f03aebb0984
Caio <120686918+Szerman08@users.noreply.github.com>
2024-10-29 18:58:23 -0300

    exec-14.py

---
+++ exec-14.py
@@ -0,0 +1,10 @@
+#14) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).
+
+
+valor = float(input("Digite um valor :"))
+if valor >=0 :
+        print("O numero informado é positivo")
+else:
+        print('O numero informado é negativo')
+
+
