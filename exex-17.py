83c41eee01460c19cc95774a0cde723778cf406f
Caio <120686918+Szerman08@users.noreply.github.com>
2025-05-21 18:59:20 -0300

    exex-17.py

--- exex-17.py
+++ exex-17.py
@@ -1,9 +1,10 @@
 17) Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
 
+ano_atual = int(input("Digite o ano atual: "))
+nascimento = int(input("Digite o ano de seu nascimento: "))
 
-nome = str(input("Digite seu nome: "))
-data = int(input("Digite o ano atual: "))
-nascimento = int(input("Digite o ano do seu nascimento: "))
-
-soma = data-nascimento
-print("Olá", nome, "a sua idade é:",soma)
+soma = ano_atual - nascimento
+if soma >= 16:
+    print("Você pode votar!")
+else:
+    print("Você não pode votar")
